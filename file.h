4a1dce626ff44d2418d0d30a404404632966f4d1
EternallyAscend <Eternally-Ascend@Outlook.com>
2020-12-06 22:48:26 +0800

    Add report file output function.

--- file.h
+++ file.h
@@ -9,16 +9,22 @@
 
 #include <stdio.h>
 
-char* getFileName(int time, char* tail);
+char* getFileName(int time, const char* tail);
 
 FILE* generateLEX();
 
 FILE* generateYACC();
 
+FILE* generateREPORT();
+
 void appendLEX(char* content);
 
 void appendYACC(char* content);
 
+void appendREPORT(char* content);
+
 void closeLEX();
 
 void closeYACC();
+
+void closeREPORT();
