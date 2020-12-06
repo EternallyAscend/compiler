c07594aa4735559e78188d191f8234e87c02929e
EternallyAscend <EternallyAscend@Outlook.com>
2020-12-06 09:12:56 +0800

    Modify the generate.sh for using on CentOS Server.

--- generate.sh
+++ generate.sh
@@ -5,6 +5,6 @@ rm lex.yy.c
 rm compiler
 yacc -d c.y
 lex c.l
-g++ lex.yy.c y.tab.c file.c table.cpp tree.cpp item.cpp -o compiler -lm -ll
+g++ lex.yy.c y.tab.c file.c table.cpp tree.cpp item.cpp -o compiler -lm
 # g++ lex.yy.c c.tab.c file.c table.cpp tree.cpp item.cpp -o compiler -lm -ll
 ./compiler example.c
