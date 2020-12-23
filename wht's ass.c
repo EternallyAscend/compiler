03eee93ba9d49ecfd31e3f8fbf914a645f1f52b4
wht <2867686623@qq.com>
2020-12-23 17:56:53 +0800

    add div

--- wht's ass.c
+++ wht's ass.c
@@ -7,10 +7,13 @@
 
 int lockOrNot = 0;
 int subCount = 0;
+int multiCount = 0;
+int divCount = 0;
 
 char* mov = "MOV";
 char* add = "ADD";
 char* sub = "SUB";
+char* imul = "IMUL";
 char* cmp = "CMP";
 char* idiv ="IDIV";
 char* jnb = "JNB";
@@ -19,7 +22,7 @@ char* ax = "eax";
 char* bx = "ebx";
 char* cx = "ecx";
 char* zf = "zf";
-char* ah = "ah";
+char* al = "al";
 char* cf = "cf";
 
 char* assSub(char* num1, char* num2) {
@@ -42,6 +45,46 @@ char* assSub(char* num1, char* num2) {
     return count;
 }
 
+char* assMulti(char* num1, char* num2) {
+    while(lockOrNot == 1) {
+        sleep(1);
+    }
+    lockOrNot = 1;
+    char* count;
+    char* firMove;
+    char* secMove;
+    char* firMulti;
+    char* storageResult;
+    sprintf(count, "multiCount%d", multiCount);
+    sprintf(firMove, "%s %s, %d", mov, ax, num1);//put num1 to ax
+    sprintf(secMove, "%s %s, %d", mov, bx, num2);
+    sprintf(firMulti, "%s %s, %s", imul, ax, bx)
+    sprintf(storageResult, "%s [%s], %s", mov, count, ax);//put ax to stroge
+    multiCount++;
+    lockOrNot = 0;
+    return count;
+}
+
+char* assDiv(char* num1, char* num2) {
+    while(lockOrNot == 1) {
+        sleep(1);
+    }
+    lockOrNot == 1;
+    char* count;
+    char* firMove;
+    char* secMove;
+    char* firDiv;
+    char* storageResult;
+    sprintf(count, "divCount%d", divCount);
+    sprintf(firMove, "%s %s, %d", mov, ax, num1);
+    sprintf(secMove, "%s %s, %d", mov, bx, num2);
+    sprintf(firDiv, "%s %s", idiv, bx);
+    sprintf(storageResult, "%s [%s], %s", mov, count, al);
+    divCount++;
+    lockOrNot = 0;
+    return count;
+}
+
 char* assPow(char* num1, char* num2) {
     while(lockOrNot == 1) {
         sleep(1);
@@ -52,3 +95,4 @@ char* assPow(char* num1, char* num2) {
     lockOrNot = 0;
     return count;
 }
+
